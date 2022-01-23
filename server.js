0b30ca4acd906396970f2fac17fa34b1de629711
Rodrigo Lira <rodrigocliras@gmail.com>
2022-01-23 19:47:11 -0300

    adicionando sequelize e removendo mongoose

--- server.js
+++ server.js
@@ -7,29 +7,21 @@
 
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var Sequelize = require('sequelize');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var port = 8000;
 var employee = require('./app/routes/employee');
+var employee_model = require('./app/models/employee');
+const { Op } = require("sequelize");
+
 var config = require('config'); // aqui estaremos carregando a localização da base de dados através dos arquivos JSON.
-
-//Opção das base de dados:
-var options = { 
-                server:{ socketOptions: {keepAlive: 1, connectTimeoutMS: 30000 }}, 
-                replset:{ socketOptions: {keepAlive: 1, connectTimeoutMS: 30000 }} 
-              };
-
-//Conexão com a base de dados:
-mongoose.connect(config.DBHost, options);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro ao conectar com a Base de Dados....: '));
 
 //Essa parte do código estaremos mostrando os logs quando acontecer os testes:
 if(config.util.getEnv('NODE_ENV') !== 'test') {
 
     //Aqui estamos usando 'morgan'. Ele é responsável por realizar as requisições de logger no middleware para Node.Js
-    app.use(morgan('combined')); 
+    app.use(morgan('combined'));
 }
 
 app.use(bodyParser.json());
@@ -54,5 +46,3 @@ app.listen(port);
 console.log("Aplicação executando na porta " + port);
 
 module.exports = app;
-
-
